816a36099364a1ac7213329bef5a8717c1af011b
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-03 14:22:02 -0800

    except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-output = ''.join(chr(i) for i in range(ord('a'), ord('z') + 1)\
+output = ''.join(chr(i) for i in range(ord('a'), ord('z') + 1)
 if chr(i) not in {'q', 'e'})
 print("{}".format(output), end='')
