fb4c9b462e0388fee626c04bdedd4aad78b32654
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-03 16:08:37 -0800

    print doubles

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}{:02d}".format(i,j), end=", " if i< 8 and j < 9 else "\n")
+for num1 in range(0, 10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+        else:
+            print("{}{}".format(num1, num2), end='')
