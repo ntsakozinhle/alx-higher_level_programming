a05a9df3396afbf7d50bc81be3f40c3a687e8a36
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-09 09:01:11 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-import py_compile
+if __name__ == "__main__":
+    from py_compile import compile
+    def print_name(file_path):
+        try:
+            compiled = compile(file_path)
+            code_object = compile.read_compiled_file(compiled)
+            names = [name for name in dir(code_object)
+                    if not name.startswith('__')]
+            name.sort()
+            for name in names:
+                print(name)
+        except compile.PyCompileError as e:
+            print(f"Error compiling {file_path}: {e}")
 
-def print_name(file_path):
-    try:
-        compiled = py_compile.compile(file_path)
-        code_object = py_compile.read_compiled_file(compiled)
-
-        names = [name for name in dir(code_object)
-                if not name.startswith('__')]
-        name.sort()
-
-        for name in names:
-            print(name)
-
-    except py_compile.PyCompileError as e:
-        print(f"Error compiling {file_path}: {e}")
-
-print_name('hidden_4.pyc')
+    print_name('hidden_4.pyc')
