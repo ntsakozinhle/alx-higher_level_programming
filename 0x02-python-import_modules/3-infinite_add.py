a32f0baf21da943496a018a270678bd809a586ef
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-09 08:49:19 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import sys
-
-argv = sys.argv[1:]
-result = sum(int(arg) for arg in argv)
-
-print(result)
+if __name__ == "__main__":
+    from sys import argv
+    argv = argv[1:]
+    result = sum(int(arg) for arg in argv)
+    print(result)
