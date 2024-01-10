37531b12880d63dbf1a97734750759e1021531c3
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-10 06:02:44 -0800

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    result_list = my_list[:idx] + my_list[idx+1:]
+    result_list = my_list.copy()
+    del result_list[idx]
     return result_list
