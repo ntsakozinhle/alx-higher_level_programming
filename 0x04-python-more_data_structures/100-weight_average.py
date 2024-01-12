84741bfeb58f7b0d2a313b47c93af76c4eb65812
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-11 21:35:47 -0800

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,6 @@ def weight_average(my_list=[]):
     weighted_sum = sum(score * weight for score, weight in my_list)
     total_weight = sum(weight for _, weight in my_list)
 
-    weighted_average = weighted sum / total_weight
+    weighted_average = weighted_sum / total_weight
 
-    return weighted average
+    return weighted_average
