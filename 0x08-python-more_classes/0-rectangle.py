43f8ce37f13ee87e4c36c4becccf36300cd83bc6
ntsakozinhle <ntsakosambo1@gmail.com>
2024-01-29 18:57:21 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This is a Module for class Rectangle """
+
+
+class Rectangle:
+    """
+    Empty class Rectangle that defines a rectangle.
+
+    Attributes:
+        No attributes are defined in this class.
+    """
+
+    def __inti__(self):
+        """
+        Initializes an instance of the Rectangle class.
+
+        Parameters:
+            No parameters are accepted for this constructor.
+        """
+        pass
