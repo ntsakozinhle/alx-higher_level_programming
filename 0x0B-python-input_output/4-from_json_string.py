b4a736b759177ff33ba1da7eb7d31864a408f887
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-07 18:39:49 -0800

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module for converting a JSON string to a Python data straucture."""
+
+
+def from_json_string(my_str):
+    """
+    Convert a JSON string to a python data structure.
+
+    Args:
+        my_str (str): The JSON string to convert.
+
+    Returns:
+        Object: The Python data structure represented by the JSON string.
+    """
+    import json
+    return json.loads(my_str)
+
+
+if __name__ == "__main__":
+    s_my_list = "[1, 2, 3]"
+    my_list = from_json_string(s_my_list)
+    print(my_list)
+    print(type(my_list))
+
+    s_my_dict = """
+    {"is_active": true, "info": {"age": 36, "average": 3.14}. "id": 12,
+    "name": "John", "places": ["San Fracisco", "Tokyo"]}
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+
+
+    try:
+        s_my_dict = """
+        {"is_active": true, 12}
+        """
+        my_dict = from_json_string(s_my_dict)
+        print(my_dict)
+        print(type(my_dict))
+    except Exception as e:
+        print("[{} {}".format(e.__class__.__name__, e))
