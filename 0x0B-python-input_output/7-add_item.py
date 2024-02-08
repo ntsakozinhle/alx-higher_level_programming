ee2fbcb07354d8c8a451717777510e1c524fae9a
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-07 19:10:18 -0800

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script for adding all arguments to a Python list and saving them to a JSON
+file. """
+
+if __name__ == "__main__":
+    import sys
+    from 5-save_to_json_file.py import save_to_json_file
+    from 6-load_from_json_file.py import load_from_json_file
+
+    try:
+        existing_list = load_from_json_file("add_item.json")
+    except Exception:
+        existing_list = []
+
+    existing_list.extend(sys.argv[1:])
+
+    save_to_json_file(existing_list, "add_item.json")
