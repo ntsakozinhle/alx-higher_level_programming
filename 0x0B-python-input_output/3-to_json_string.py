9ab89a4837b690cfce35152726a6936ecacf8a7d
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-07 18:29:04 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module for converting an object to its JSON representation"""
+
+
+def to_json_string(my_obj):
+    """
+    Convert an object to its JSON representation.
+
+    Args:
+        my_obj: The object to convert to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    import json
+    return json.dumps(my_obj)
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3]
+    s_my_list = to_json_string(my_list)
+    print(s_my_list)
+
+    my_dict = {
+        'id': 12,
+        'name': "John",
+        'places': ["San Francisco", "Tokyo"],
+        'is_active': True,
+        'info': {
+            'age': 36,
+            'average': 3.14
+        }
+    }
+    s_my_dict = to_json_string(my_dict)
+    print(s_my_dict)
+    print(type(s_my_dict))
+
+    try:
+        my_set = {132, 3}
+        s_my_set = to_json_string(my_set)
+        print(s_my_set)
+        print(type(s_my_set))
+    except Exception as e:
+        print("[{}] {}". format(e.__class__.__name__, e))
