f97cb1c46f3e63749919e3212b447c4b563ffa66
ntsakozinhle <ntsakosambo1@gmail.com>
2024-02-09 08:37:09 -0800

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,13 @@ class Square(Rectangle):
         """Initialize Square instance with size, x, y, and id"""
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Returns a string representation of the Square instance"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width if self.width == self.height else self.height)
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        self.width = value
+        self.height = value
