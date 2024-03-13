b9d2e5da7ceb4b99e4eab4fd424cf4c0306b6f7e
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-13 14:36:08 -0700

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@ USE hbtn_0c_0;
 -- Convert hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert firts_table table to UTF8
+-- Convert first_table table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert name field in first_table table UTF8
