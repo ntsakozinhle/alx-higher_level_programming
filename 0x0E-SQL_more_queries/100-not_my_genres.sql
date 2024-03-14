3d178e79431434705ecafc8f3986c7d2e47fd337
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-13 19:43:07 -0700

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,11 @@
 -- List all shows in data dump hbtn_0d_tvshows
 SELECT tv_genres.name
 FROM tv_genres
-WHERE tv_genres.id NOT IN (
-	SELECT tv_genres.id
-	FROM tv_genres
-	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-	JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
-	WHERE tv_shows.title = 'Dexter'
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_show_genres.genre_id = tv_genres.id
+	AND tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
