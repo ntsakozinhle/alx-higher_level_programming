516ecc52eed974dfd701c94db47cb6234e9a4147
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-13 18:22:32 -0700

    Always a name

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
--- Create or update MySQL database hbtn_0d_2
+-- Create or update MySQL database user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Create or update MySQL user user_0d_2 and grant SELECT privileg
+-- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Flush privileges
