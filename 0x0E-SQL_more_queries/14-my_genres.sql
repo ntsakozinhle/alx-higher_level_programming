18bec07af12115931316dba29973f11e01a729a9
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-13 19:24:29 -0700

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- List all shows in data dump hbtn_0d_tvshows
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
