a16217c45c38af6ae9811d960aa6b0895d1ec320
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-09 20:00:28 -0700

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nb0ccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const element of list) {
     if (element === searchElement) {
