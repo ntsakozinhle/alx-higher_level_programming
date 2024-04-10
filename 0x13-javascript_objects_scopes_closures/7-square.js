c64ab2b2edeebaa6e9a12369be98a5c57b202b88
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-09 19:51:07 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/7-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nb0ccurences = function (list, searchElement) {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
