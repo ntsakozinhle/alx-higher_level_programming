b789b633cfb232fdcfa4ffa79a5652a1db0bb848
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-12 16:12:18 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,25 +2,44 @@
 import MySQLdb
 import sys
 
+def list_states(username, password, database):
+   conn = None
+   cur = None
+   try:
+        
+        conn = MySQLdb.connect(host="localhost", port=3306,
+                                user=username, passwd=password,
+                                db=database, charset="utf8")
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+        rows = cur.fetchall()
+
+        seen_states = set()
+
+        for row in rows:
+            state_id, state_name = row
+            if state_name not in seen_states:
+                seen_states.add(state_name)
+                print(row)
+
+   except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+   finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    unique_states = set()
-
-    results = cursor.fetchall()
-
-    for row in results:
-        state_id, state_name = row
-        if state_name not in unique_states:
-            unique_states.add(state_name)
-            print(row)
-
-    db.close()
+    list_states(username, password, database)
