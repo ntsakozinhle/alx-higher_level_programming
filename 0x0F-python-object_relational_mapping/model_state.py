fe493a4278afc9fa272a9f8e5e533f0c9864530f
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-14 21:24:58 -0700

    First state model ~ lost

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+import sys
+
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    a class that represents the 'states' table in the database.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        """
+        Return a string representaton of State instance
+        """
+        return f"<State(id={self.id}, name='{self.name}')>"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python3 6-model_state.py <usernme> <password> <database_name>")
+        sys.exit(1)
+
+    db_user, db_pass, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    db_ip = 'localhost'
+
+    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pass}@{db_ip}/{db_name}', pool_pre_ping=True)
+
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    Base.metadata.create_all(engine)
