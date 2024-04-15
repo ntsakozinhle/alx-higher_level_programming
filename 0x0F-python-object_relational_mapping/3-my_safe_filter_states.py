b5809305a9eb1b0004a8dcccef2b7869674e2bbc
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-14 20:38:47 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,47 +1,33 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database and lists all states whose match
+argument
+"""
+
+
 import MySQLdb
 import sys
 
+def filter_states_safely(user, password, db_name, state_name):
+    """
+    Connects to the specified MySQL database and lists all states whose name
+    matches the argument
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
 
-def safe_filter_states(username, password, database, state_name):
-    try:
-        # Connect to the MySQL server
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
-        # Create a cursor object
-        cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
 
-        # Construct the SQL query with parameterized query
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cur.execute(query, (state_name,))
+    query_rows = cur.fetchall()
 
-        # Fetch all rows
-        rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-        # Fetch all rows
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    finally:
-        # Close the cursor and connection
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
+    cur.close()
+    conn.close()
 
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+    user, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    # Extract arguments from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Call the function to filter states
-    safe_filter_states(username, password, database, state_name)
+    filter_states_safely(user, password, db_name, state_name)
