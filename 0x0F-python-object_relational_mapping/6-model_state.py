a4c0048c8fabcdbc29db6648326e21d693d6070c
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-19 11:42:38 -0700

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -4,8 +4,30 @@
 import sys
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def display_records():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+
+    for state in states:
+        print(state)
+
+    session.close()
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    if len (sys.argv) != 4:
+        print ("Usage: python3 6-model_state.py <username> <password> <database>")
+        sys.exit(1)
+
+    db_user, db_pass, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    db_ip = 'localhost'
+
+    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pass}@{db_ip}/{db_name}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
+    display_records()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_utils import database_exists, create_database
-import sys
 
 
 Base = declarative_base()
@@ -12,6 +9,7 @@ class State(Base):
     a class that represents the 'states' table in the database.
     """
     __tablename__ = 'states'
+
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
@@ -23,16 +21,12 @@ class State(Base):
         return f"<State(id={self.id}, name='{self.name}')>"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 6-model_state.py <usernme> <password> <database_name>")
+    if len (sys.argv) != 4:
+        print ("Usage: python3 model_state.py <username> <password> <database>")
         sys.exit(1)
 
     db_user, db_pass, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
     db_ip = 'localhost'
 
-    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pass}@{db_ip}/{db_name}', pool_pre_ping=True)
-
-    if not database_exists(engine.url):
-        create_database(engine.url)
-
+    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_pass}@(db_ip)/{db_name}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
