40c3fc5f10a1c277e684281d8f40991ee82fb4da
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-25 19:03:04 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,13 @@
 # this script takes a URL as input, sends a request to the URL using curl
 # in silent mode, and displays the size of the body of the response in bytes.
 
+# Check is a URL is provided
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+# URL to fetch
+URL="$1"
+
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
