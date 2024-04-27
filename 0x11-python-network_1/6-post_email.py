1d88b678aa877fba41b571c1fecf14075ce9339e
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-26 20:09:59 -0700

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module to send a POST request to a URL with an email as parameter
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
