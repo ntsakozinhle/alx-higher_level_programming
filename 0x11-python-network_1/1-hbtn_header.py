85d998b372c8120bf7a86ce090cdc2db7165ab58
ntsakozinhle <ntsakosambo1@gmail.com>
2024-04-26 19:04:30 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+Module to send a request to a URL and display the value
+"""
+
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        header = response.getheader('X-Request-ID')
+        print(header)
